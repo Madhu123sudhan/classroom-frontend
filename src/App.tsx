0732e81528730dbf7809581020687715208c8c96
Madhu123sudhan <madhusudhan93436@gmail.com>
2026-08-07 15:48:31 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import {
   Refine,
-  GitHubBanner,
-  WelcomePage,
-  Authenticated,
+  GitHubBanner
 } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import Dashboard from "./pages/Dashboard";
 
 import { BrowserRouter, Route, Routes, Outlet } from "react-router";
 import routerProvider, {
@@ -15,16 +14,19 @@ import routerProvider, {
   DocumentTitleHandler,
 } from "@refinedev/react-router";
 import { dataProvider } from "./providers/data";
-import { Login } from "./pages/login";
-import { Register } from "./pages/register";
-import { ForgotPassword } from "./pages/forgot-password";
+// import { Login } from "./pages/login";
+// import { Register } from "./pages/register";
+// import { ForgotPassword } from "./pages/forgot-password";
 import { ErrorComponent } from "./components/refine-ui/layout/error-component";
 import { Layout } from "./components/refine-ui/layout/layout";
 import { Header } from "./components/refine-ui/layout/header";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { Toaster } from "./components/refine-ui/notification/toaster";
-import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
+import { ThemeProvider } from "./components/refine-ui/theme/theme-provider"
 import "./App.css";
+import {Home, BookOpen} from "lucide-react";
+import SubjectsCreate from "@/pages/subjects/create.tsx";
+import SubjectsList from "@/pages/subjects/list.tsx";
 
 function App() {
   return (
@@ -42,9 +44,32 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: "nAP68m-6MhjgA-dOvUJu",
               }}
+              resources={[
+                {
+                  name: 'dashboard',
+                  list: '/',
+                  meta: { label: 'Home', icon: <Home />}
+                },
+                {
+                  name: 'subjects',
+                  list: '/subjects',
+                  create: '/subjects/create',
+                  meta: { label: 'Subjects', icon: <BookOpen />}
+                }
+              ]}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route element={
+                  <Layout>
+                    <Outlet/>
+                  </Layout>
+                }>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="subjects">
+                    <Route index element={<SubjectsList />} />
+                    <Route path="create" element={<SubjectsCreate />} />
+                  </Route>
+                </Route>
               </Routes>
               <Toaster />
               <RefineKbar />
---
+++ src/pages/subjects/list.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const SubjectsList = () => {
+    return (
+        <div>Subjects - List</div>
+    )
+}
+export default SubjectsList
